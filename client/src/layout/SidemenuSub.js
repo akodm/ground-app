fcd7570b505aa9960eacaa854077ef1e6430a4f1
조준명 <a8456452@naver.com>
2020-08-29 15:20:11 +0900

    사이드 메뉴 아이콘 삭제 및 로그아웃 추가

--- client/src/layout/SidemenuSub.js
+++ client/src/layout/SidemenuSub.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+import config from '../client-config';
 
 function SidemenuSub(props) {
-    const { setOpen, id } = props;
+    const { setOpen, id, setUser } = props;
+
+    const logout = async() => {
+        try {
+            await axios.get(`${config.server}/logout?id=${id}`);
+
+            setUser(null);
+            localStorage.removeItem("ground_user");
+
+            alert("로그아웃 되었습니다.");
+            window.location.href = "/";
+        } catch(err) {
+            alert("로그아웃 시도 중 에러가 발생했습니다.");
+        };
+    }
+
     return (
         <div className="sidemenu-sub">
             <div className="sidemenu-sub-overlay" onClick={() => setOpen(false)}></div>
             <div className="sidemenu-sub-menu">
-                <a href="/"><img alt="menu" src="/image/home.png"></img></a>
-                <Link to="/map" onClick={() => setOpen(false)}><img alt="menu" src="/image/map.png"></img></Link>
-                <Link to={`/profile/${id}`} onClick={() => setOpen(false)}><img alt="menu" src="/image/profile.png"></img></Link>
+                <a href="/"><div className="sidemenu-sub-item">홈</div></a>
+                <Link to="/map" onClick={() => setOpen(false)}><div className="sidemenu-sub-item">지도</div></Link>
+                <Link to={`/profile/${id}`} onClick={() => setOpen(false)}><div className="sidemenu-sub-item">프로필</div></Link>
+                <div onClick={logout} className="sidemenu-sub-item">로그아웃</div>
             </div>
         </div>
     );
