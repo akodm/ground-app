fdd76258f8f16de997d3b9feda5d2bb93acd45dd
조준명 <a8456452@naver.com>
2020-09-02 14:42:10 +0900

    지도 맵 컴포넌트 내용 전체 바꾸는 중

--- client/src/page/MapComponent.js
+++ client/src/page/MapComponent.js
@@ -1,35 +1,11 @@
-import React, { Component } from 'react';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
-import config from '../client-config';
+import React from 'react';
 
-class MapComponent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-
-        };
-    }
-
-    render() {
-        const { google, loaded } = this.props;
-        return loaded && (
-            <div className="map">
-                <Map
-                    google={google}
-                    zoom={16}
-                    initialCenter={{ lat: 37.401815, lng: 126.922779}}
-                >
-                    <Marker
-                        title={'daelim univercity'}
-                        name={'daelim'}
-                        position={{lat: 37.401989, lng: 126.922571}} />
-                </Map>
-            </div>
-        )
-    }
+function MapComponent(props) {
+    return (
+        <div id="map" style={{width:"100%", height:"100%"}}>
+            
+        </div>
+    );
 }
 
-export default GoogleApiWrapper({
-    apiKey: config.apiKey,
-    language : "ko",
-})(MapComponent);
+export default MapComponent;
