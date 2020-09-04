876898420ba7a1746732b8c084c85718f05d908a
조준명 <a8456452@naver.com>
2020-09-04 09:05:53 +0900

    처음 구글지도를 로드하고 난 뒤에 베이스js를 열도록 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 import './css/Base.css';
@@ -8,11 +8,31 @@ import './css/Map.css';
 import './css/Button.css';
 import './css/Login.css';
 import './css/Textfield.css';
+import './css/AddMarker.css';
+import './css/MarkerPopup.css';
+import './css/LoadMask.css';
 
 import Base from './layout/Base';
 
 export default function App() {
-    return (
+    const [ maps, setMaps ] = useState(null);
+
+    useEffect(() => {
+        if(maps) { return () => clearInterval(interval); }
+        const interval = setInterval(() => {
+            initMap();
+        }, 100);
+
+        return () => clearInterval(interval);
+    }, [maps]);
+
+    function initMap() {
+        const loadMap = window.google || false;
+
+        if(loadMap) { setMaps(window.google); }
+    }
+
+    return maps ? (
         <Base />
-    );
+    ) : <div></div>
 }
